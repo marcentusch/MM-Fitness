67447e1ead009a9db97aeb3ccc2c469a4780ff41
christianflemmer <flemmer94@hotmail.com>
2017-10-30 11:56:45 +0100

    basic footer, header, login, homepage

--- main.js
+++ main.js
@@ -7,11 +7,18 @@ app.use(express.static('public'));
 app.use(bodyparser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-
+// Root route [Login]
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+// Min side-route
+app.get('/home', (req, res) => {
+	res.render('home');
+});
+
+
+// Server listening
 var port = 8080;
 app.listen(port, () => {
     console.log("Server listening on port " + port);
