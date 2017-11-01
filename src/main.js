bf2da4efe5a1956419ef9566aee330895f46aca3
christianflemmer <flemmer94@hotmail.com>
2017-11-01 11:52:28 +0100

    basic database setup

--- src/main.js
+++ src/main.js
@@ -2,10 +2,36 @@ var bodyparser = require('body-parser');
 var Chart = require('chart.js');
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
 var config = require('../config/global.config.json');
 var userData = require('./data/users.json');
 
+// Connect to database
+mongoose.connect('mongodb://localhost/mm_fitness_app');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+    //console.log("CONNECTED TO DB!");
+});
+
+// Define schema for user
+var userSchema = mongoose.Schema({
+    name: String,
+    email: String
+});
+
+// Compile schema into model
+var User = mongoose.model('User', userSchema);
+
+// Create a user
+/*User.create({name: 'Johnny', email: 'johnny@johnny.dk'}, function(err, user){
+    if(err){
+        console.log(err);
+    }
+    console.log(user);
+}); */
 
 // Setup
 app.use(express.static('public'));
@@ -43,6 +69,11 @@ app.get('/nyheder', (req, res) => {
     res.render('nyheder');
 });
 
+// Opdatér vægt route
+app.post('/home/:id', (req, res) => {
+    let newWeight = req.body.newWeight;
+
+});
 
 // Server listening
 app.listen(config.port, () => {
