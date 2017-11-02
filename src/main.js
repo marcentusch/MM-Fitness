264545be77cb9edb5772ddd7817cb7a334785094
marcentusch <marcentusch@hotmail.com>
2017-11-02 10:58:42 +0100

    added function for creating test users

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const Chart = require('chart.js');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const config = require('../config/global.config.json');
 const userFactory = require('./services/userFactory.js');
@@ -30,8 +31,13 @@ app.get('/', (req, res) => {
 });
 
 // Hjem
-app.get('/home', (req, res) => {
-    res.render('home', {users: userData});
+app.get('/home', async (req, res) => {
+    try {
+        const user = await userFactory.findUser("johnny@johnny.dk", User);
+        res.render('home', {user: user});
+    } catch(err) {
+        throw err;
+    }
 });
 
 // Profil
@@ -70,16 +76,50 @@ app.post('/home/:id', (req, res) => {
 
 });
 
-app.post('/user/new', (req,res) => {
-    const body = req.body;
-    const user = {
-        name: body.name,
-        email: body.email,
-        password: body.password,
-        avatarURL: body.avatarURL
+
+function randomUser() {
+    const names = ["Jens", "Brian", "Søren", "Ole", "Denise", "Maibrit"];
+    const emails = ["foo@bar.dk", "test@test.dk", "lorem@ipsum.dk"];
+    const password = "12345";
+    const avatarURL = "http://www.qygjxz.com/data/out/190/5691490-profile-pictures.png";
+    
+    const weights =[];
+    for(let i = 0; i < 10; i++) {
+        let weight = {
+            date: randomDate(),
+            weight: randomNumber(50,100, 1)
+        }
+        weights.push(weight);
     }
+
+    const weightStats = {
+        currentWeight: randomNumber(50, 100, 1),
+        startWeight: randomNumber(50, 100, 1),
+        weightProgress: randomNumber(0, 10, 1),
+        allWeights: weights
+    }
+    const user = {
+        name: names[randomNumber(0,5, 0)],
+        email: emails[randomNumber(0,2)],
+        password: password,
+        avatarURL: avatarURL,
+        weightStats: weightStats
+    }
+    console.log(JSON.stringify(user, null, 3));
     userFactory.createNewUser(user, User);
-});
+}
+randomUser();
+
+function randomDate() {
+    const someDate = randomNumber(1,27) + "-" + randomNumber(1, 12) + "-2017";
+    const randomDate = moment(someDate, "D-M-YYYY").format("d/M/YY");
+    return randomDate;
+}
+
+function randomNumber(min, max, decimals) {
+    let randomNumber = Math.random() * (max - min) + min;
+    return randomNumber.toFixed(decimals);
+}
 
 
 
