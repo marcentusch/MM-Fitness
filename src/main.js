8a623341ca27770fe9925c9435935d34c5309c53
christianflemmer <flemmer94@hotmail.com>
2017-11-06 14:29:24 +0100

    user authentication wip

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-const bodyparser = require('body-parser');
-const Chart = require('chart.js');
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
+const bodyparser = require('body-parser'),
+      Chart = require('chart.js'),
+      express = require('express'),
+      app = express(),
+      mongoose = require('mongoose'),
+      passport = require('passport'),
+      LocalStrategy = require('passport-local').Strategy,
+      passportLocalMongoose = require('passport-local-mongoose');
 
 const config = require('../config/global.config.json');
 const userFactory = require('./services/userFactory.js');
@@ -10,29 +13,40 @@ const userData = require('./schemas/userSchema.js');
 
 // MAKING AUTH FROM THIS: https://scotch.io/tutorials/easy-node-authentication-setup-and-local
 
-// Setup
-app.use(express.static('public'));
-app.use(bodyparser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
-
-
 // Database stuff
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/mm_fitness_app', {useMongoClient: true});
 const db = mongoose.connection;
 
+// Make user-schema
 const userSchema = mongoose.Schema(userData);
+userSchema.plugin(passportLocalMongoose);
 const User = mongoose.model('User', userSchema);
 
+// Setup
+app.use(express.static('public'));
+app.use(bodyparser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(require('express-session')({
+    secret: "MM-Fitness er den vildeste app nogensinde!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Serialize & deserialize the user in the session
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // Uncomment this method for test data, specify amount of users
-//userFactory.randomUser(User, 35);
+//userFactory.randomUser(User, 1);
 
-// Login
-app.get('/', (req, res) => {
-    res.render('login');
-});
+// ===============================================================
+// ROUTES
 
-// Hjem
+/* // Hjem
 app.get('/home/:_id', async (req, res) => {
     try {
         const user = await userFactory.findUser(req.params._id, User);
@@ -40,39 +54,45 @@ app.get('/home/:_id', async (req, res) => {
     } catch(err) {
         throw err;
     }
+}); */
+
+// Root route
+app.get('/', (req, res) => {
+    res.send('Root route')
 });
 
-// Profil
-app.get('/profile', async (req, res) => {
-    try {
-        const user = await userFactory.findUser("test@test.dk", User);
-        res.render('profile', {user: user});
-    } catch(err) {
-        throw err;
-    }
+app.get('/home', isLoggedIn, (req, res) => {
+    let user = req.user;
+    res.render('home', {user: user});
 });
 
-// Trænings program
+// Profile
+app.get('/profile', isLoggedIn, (req, res) => {
+    let user = req.user;
+    res.render('profile', {user: user})
+});
+
+// Training program
 app.get('/program', (req, res) => {
     res.render('program');
 });
 
-// Kost
+// Meal plan
 app.get('/meal-plan', (req, res) => {
     res.render('meal-plan');
 });
 
-// Indbakke
+// Inbox
 app.get('/inbox', (req, res) => {
     res.render('inbox');
 });
 
-// Nyheder
+// News
 app.get('/news', (req, res) => {
     res.render('news');
 });
 
-// Opdatér vægt route
+// Update weight route
 app.post('/update/:_id/weight', async (req, res) => {
     try {
         const userId = req.params._id;
@@ -84,6 +104,58 @@ app.post('/update/:_id/weight', async (req, res) => {
         throw(err);
     }
 });
+
+// ===============================================================
+// AUTH ROUTES
+
+// Show signup form
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+// Handling user signup
+app.post('/register', (req, res) => {
+    req.body.username;
+    req.body.password;
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate('local')(req, res, function(){
+            res.redirect('/home')
+        });
+    });
+});
+
+// ===============================================================
+// LOGIN ROUTES
+
+// Render login form
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// Login logic w. middleware
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/login'
+}), (req, res) => {
+
+});
+
+// Logout route
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
 
 // Server listening
 app.listen(config.port, () => {
